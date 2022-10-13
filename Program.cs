d4b680dd0ec84704abc90c0d1135d16a90abcb5c
StarGrim33 <prosky33@gmail.com>
2022-10-13 18:15:51 +0300

    1
    First commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HomeWorkFunction3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ToConvert();
+        }
+
+        static void ToConvert()
+        {
+            bool isProgramOn = true;
+
+            while (isProgramOn)
+            {
+                Console.WriteLine("Введите строку: ");
+                string? userInput = Console.ReadLine();
+                bool result = int.TryParse(userInput, out int number);
+
+                if (result == true)
+                {
+                    Console.WriteLine("Successful: " + number);
+                    isProgramOn = false;
+                }
+                else
+                    Console.WriteLine("Try again");
+            }
+
+        }
+
+    }
+}
