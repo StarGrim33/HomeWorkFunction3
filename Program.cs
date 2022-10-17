a7041e002a13e27c9c15eaa7d61eb354de8f2e18
StarGrim33 <prosky33@gmail.com>
2022-10-17 17:03:43 +0300

    5
    Fifth commit

--- Program.cs
+++ Program.cs
@@ -1,40 +1,39 @@
 ﻿namespace HomeWorkFunction3
 {
     internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+
+
+        ReadInt();
+    }
+
+    static int ReadInt()
+    {
+        int number = 0;
+        bool isProgramOn = true;
+
+        while (isProgramOn)
         {
-            
+            Console.WriteLine("Введите значение: ");
+            string? userInput = Console.ReadLine();
+            bool haveResult = int.TryParse(userInput, out number);
 
-            ReadInt();
-        }
-
-        static int ReadInt()
-        {
-            int number = 0;
-            bool isProgramOn = true;
-
-            while (isProgramOn)
+            if (haveResult == true)
             {
-                Console.WriteLine("Введите значение: ");
-                string? userInput = Console.ReadLine();
-                bool haveResult = int.TryParse(userInput, out number);
-
-                if (haveResult == true)
-                {
-                    Console.WriteLine("Введенное число: " + number);
-                    return number;
-                }
-                else
-                {
-                    Console.WriteLine("Введенная строка: " + userInput);
-                }
-
+                Console.WriteLine("Введенное число: " + number);
+                return number;
             }
-            return number;
-
+            else
+            {
+                Console.WriteLine("Введенная строка: " + userInput);
+            }
         }
+        return number;
 
     }
+
+}
 
 }
